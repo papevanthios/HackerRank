269444254968b873b312e1e4d4080ae989aace7b
Evanthios Papadopoulos <papevanthios@gmail.com>
2021-02-26 16:39:51 +0200

    changes

---
+++ .history/javaFiles/newFile_20210226163810.java
@@ -0,0 +1,15 @@
+package javaFiles;
+
+/**
+* The newFile.java program implemented in Java for learning purposes.
+*
+* @author Evanthios Papadopoulos
+* @since 2/26/2021
+*/ 
+
+public class newFile {
+    public static void main(String[] args) {
+        System.out.print("\033\143");
+
+    }
+}
---
+++ javafiles/WelcomeToJava.java
@@ -0,0 +1,16 @@
+package javafiles;
+
+/**
+* The WelcomeToJava.java program implemented in Java for learning purposes.
+*
+* @author Evanthios Papadopoulos
+* @since 2/26/2021
+*/ 
+
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.print("\033\143");
+
+    }
+    
+}
